079098a4f1940a99063fa36ef1c221404c14c14e
Olle Kaiser <olle.kaiser@learnet.se>
2019-11-14 13:05:28 +0100

    Added ratebar and globals

--- app/lib/FrontPage.dart
+++ app/lib/FrontPage.dart
@@ -1,7 +1,10 @@
-import 'package:fl_chart/fl_chart.dart';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'colors.dart';
 import 'RestaurantWidget.dart';
+import 'globals.dart' as globals;
 
 class FrontPage extends StatelessWidget {
   Map restaurants;
@@ -25,10 +28,9 @@ class FrontPage extends StatelessWidget {
                 RestaurantWidget(restaurants['greek']),
                 RestaurantWidget(restaurants['olearys']),
                 Padding(
-                  child: Chart(restaurants['olearys'].color,
-                      restaurants['olearys'].darkColor),
-                  padding: EdgeInsets.all(20),
-                )
+                  child: RateBar(restaurants['olearys'].color),
+                  padding: EdgeInsets.only(top: 20),
+                ),
               ],
             )
           ],
@@ -38,59 +40,81 @@ class FrontPage extends StatelessWidget {
   }
 }
 
-class Chart extends StatefulWidget {
-  Color primary, secondary;
-  Chart(this.primary, this.secondary);
+class RateBar extends StatefulWidget {
+  Color color;
+
+  RateBar(this.color);
   @override
-  _ChartState createState() => _ChartState();
+  _RateBarState createState() => _RateBarState();
 }
 
-class _ChartState extends State<Chart> {
+class _RateBarState extends State<RateBar> {
+  int selected = -1;
+
   @override
   Widget build(BuildContext context) {
-    double width = MediaQuery.of(context).size.width;
-    return Container(
-      alignment: Alignment.center,
-      color: Colors.white,
-      child: LineChart(
-        LineChartData(
-          gridData: FlGridData(show: false),
-          titlesData: FlTitlesData(show: false),
-          borderData: FlBorderData(show: false),
-          minX: 0,
-          maxX: 11,
-          minY: 0,
-          maxY: 6,
-          lineBarsData: [
-            LineChartBarData(
-              spots: const [
-                FlSpot(0, 1),
-                FlSpot(2.6, 1),
-                FlSpot(4.9, 1),
-                FlSpot(6.8, 3),
-                FlSpot(8, 1),
-                FlSpot(9.5, 1),
-                FlSpot(11, 1),
+    return Center(
+      child: Container(
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(20),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(.1),
+                  blurRadius: 10,
+                )
+              ]),
+          width: MediaQuery.of(context).size.width - 50,
+          child: Padding(
+            child: Row(
+              children: <Widget>[
+                Smiley(3, widget.color),
+                Smiley(2, widget.color),
+                Smiley(1, widget.color),
+                Smiley(0, widget.color),
               ],
-              isCurved: true,
-              colors: [widget.primary, widget.secondary],
-              barWidth: 4,
-              isStrokeCapRound: true,
-              dotData: const FlDotData(
-                show: false,
-                dotColor: Colors.red,
-              ),
-              belowBarData: BarAreaData(
-                show: true,
-                colors: [
-                  widget.primary.withOpacity(.3),
-                  widget.secondary.withOpacity(.3)
-                ],
-              ),
             ),
-          ],
+            padding: EdgeInsets.all(20),
+          )),
+    );
+  }
+}
+
+class Smiley extends StatefulWidget {
+  int value;
+  List smileys = [
+    Icons.sentiment_dissatisfied,
+    Icons.sentiment_neutral,
+    Icons.sentiment_satisfied,
+    Icons.sentiment_very_satisfied
+  ];
+
+  List colors = [];
+
+  Color color;
+
+  Smiley(this.value, this.color);
+
+  @override
+  _SmileyState createState() => _SmileyState();
+}
+
+class _SmileyState extends State<Smiley> {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      child: Container(
+        child: Icon(
+          widget.smileys[widget.value],
+          color: widget.color,
+          size: (MediaQuery.of(context).size.width - 50 - (10 * 4)) / 4,
         ),
       ),
+      onTap: () {
+        setState(() {
+          widget.color = Colors.red;
+        });
+      },
     );
   }
 }
---
+++ app/lib/globals.dart
@@ -0,0 +1,1 @@
+library appetize.globals;
