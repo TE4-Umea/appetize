60d102c0bfb774223a2ca148241d6cc979b47f62
Olle Kaiser <olle.kaiser@learnet.se>
2019-11-26 11:28:06 +0100

    Updated database

--- appetize.sql
+++ appetize.sql
@@ -13,12 +13,10 @@
 
 
 -- Dumpar databasstruktur för appetize
-DROP DATABASE IF EXISTS `appetize`;
 CREATE DATABASE IF NOT EXISTS `appetize` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */;
 USE `appetize`;
 
 -- Dumpar struktur för tabell appetize.admins
-DROP TABLE IF EXISTS `admins`;
 CREATE TABLE IF NOT EXISTS `admins` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `username` varchar(50) COLLATE utf8_bin NOT NULL DEFAULT '0',
@@ -29,7 +27,6 @@ CREATE TABLE IF NOT EXISTS `admins` (
 -- Dataexport var bortvalt.
 
 -- Dumpar struktur för tabell appetize.classes
-DROP TABLE IF EXISTS `classes`;
 CREATE TABLE IF NOT EXISTS `classes` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `time` varchar(50) COLLATE utf8_bin NOT NULL DEFAULT '0',
@@ -41,7 +38,6 @@ CREATE TABLE IF NOT EXISTS `classes` (
 -- Dataexport var bortvalt.
 
 -- Dumpar struktur för tabell appetize.codes
-DROP TABLE IF EXISTS `codes`;
 CREATE TABLE IF NOT EXISTS `codes` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `code` varchar(50) COLLATE utf8_bin NOT NULL DEFAULT '0',
@@ -49,12 +45,11 @@ CREATE TABLE IF NOT EXISTS `codes` (
   `creator` int(11) NOT NULL DEFAULT '0',
   `class` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=39 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=51 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- Dataexport var bortvalt.
 
 -- Dumpar struktur för tabell appetize.forms
-DROP TABLE IF EXISTS `forms`;
 CREATE TABLE IF NOT EXISTS `forms` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `day` date DEFAULT NULL,
@@ -62,31 +57,30 @@ CREATE TABLE IF NOT EXISTS `forms` (
   `edited` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `rating` float NOT NULL,
   `text` text COLLATE utf8_bin,
+  `user` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Daily input and reviews from students are saved here.\r\n';
 
 -- Dataexport var bortvalt.
 
 -- Dumpar struktur för tabell appetize.tokens
-DROP TABLE IF EXISTS `tokens`;
 CREATE TABLE IF NOT EXISTS `tokens` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user` varchar(50) COLLATE utf8_bin NOT NULL DEFAULT '0',
   `token` varchar(250) COLLATE utf8_bin NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- Dataexport var bortvalt.
 
 -- Dumpar struktur för tabell appetize.users
-DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `appetize_id` varchar(250) COLLATE utf8_bin NOT NULL,
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `class` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=42 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- Dataexport var bortvalt.
 
