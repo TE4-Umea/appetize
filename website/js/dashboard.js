52bcefc8dc211565e58a59e244390364625db296
Olle Kaiser <olle.kaiser@learnet.se>
2019-11-27 13:27:06 +0100

    Added dashboard function

---
+++ website/js/dashboard.js
@@ -0,0 +1,54 @@
+function updateDashboard() {
+    var data = {
+        restaurants: [
+            {
+                name: "Greek Grill",
+                code_name: "greek",
+                classes: 4,
+                students: 50,
+                todays_score: 5.8,
+                data: {
+                    all: [
+                        {
+                            date: "21-12-2019",
+                            score: 5.3,
+                            food: "Hamgurger"
+                        },
+                        {
+                            date: "22-12-2019",
+                            score: 7.3,
+                            food: "Pasta"
+                        }
+                    ],
+                    veg: [],
+                    gluten: []
+                }
+            },
+            {
+                name: "O'learys",
+                code_name: "olearys",
+                classes: 4,
+                students: 50,
+                todays_score: 5.8,
+                data: {
+                    all: [
+                        {
+                            date: "21-12-2019",
+                            score: 5.3,
+                            food: "Hamgurger"
+                        },
+                        {
+                            date: "22-12-2019",
+                            score: 7.3,
+                            food: "Pasta"
+                        }
+                    ],
+                    veg: [],
+                    gluten: []
+                }
+            }
+        ]
+    };
+
+    // Update dashboard
+}
