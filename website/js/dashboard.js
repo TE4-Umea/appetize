263007813ec3e21f183c170b5a070613a894b7e7
Leo Sidebo <54834547+leosidebo@users.noreply.github.com>
2019-11-28 14:44:13 +0100

    Dashboard chart and redesign (#25)
    * Added a basic google chart with wrong mock data
    
    * Added a temporary image to help with design
    
    * Styling for the graph and support for multiple graphs
    
    * Added a separator div which works similar to a hr
    
    * Styling for the comment button link
    
    * New design for viewing user data. Shows a chart and other datapoints.
    
    * Chart placeholder and button spacing
    
    * Added buttons for choosing diets

--- website/js/dashboard.js
+++ website/js/dashboard.js
@@ -25,7 +25,7 @@ function updateDashboard() {
                 }
             },
             {
-                name: "O'learys",
+                name: "O'Learys",
                 code_name: "olearys",
                 classes: 4,
                 students: 50,
@@ -50,5 +50,47 @@ function updateDashboard() {
         ]
     };
 
-    // Update dashboard
+    google.charts.load('current', { packages: ['corechart', 'line'] });
+    google.charts.setOnLoadCallback(drawBasic);
+
+    function drawBasic() {
+
+        var data = new google.visualization.DataTable();
+        data.addColumn('number', 'Datum');
+        data.addColumn('number', 'Betyg');
+
+        data.addRows([
+            [0, 0], [1, 10], [2, 23], [3, 17], [4, 18], [5, 9],
+            [6, 11], [7, 27], [8, 33], [9, 40], [10, 32], [11, 35],
+            [12, 30], [13, 40], [14, 42], [15, 47], [16, 44], [17, 48],
+            [18, 52], [19, 54], [20, 42], [21, 55], [22, 56], [23, 57],
+            [24, 60], [25, 50], [26, 52], [27, 51], [28, 49], [29, 53],
+            [30, 55]
+        ]);
+
+        var options = {
+            width: 850,
+            height: 200,
+            tooltip: {
+                textStyle: {
+                    fontSize: 14
+                }
+            },
+            legend: 'none',
+            chartArea: {
+                left: '5%',
+                top: '5%',
+                width: '85%',
+                height: '85%',
+            }
+
+        };
+
+        let charts = document.getElementsByClassName('googleChart');
+        for (let i = 0; i < charts.length; i++) {
+            let chart = new google.visualization.LineChart(charts[i]);
+            chart.draw(data, options);
+        }
+    }
 }
+
