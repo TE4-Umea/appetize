b1c2068edc56884d8e9e4f043247936f09b3c4ad
Olle Kaiser <olle.kaiser@learnet.se>
2019-11-28 12:48:21 +0100

    Added online API Endpoint to master

--- app/lib/globals.dart
+++ app/lib/globals.dart
@@ -5,9 +5,9 @@ import 'Restaurant.dart';
 import 'package:flutter/material.dart';
 
 // ~ ONLINE SHOULD BE CHOOSEN IN MASTER BRANCH ~
-//const String API_ENDPOINT = 'https://appetize.umea-ntig.se';
+const String API_ENDPOINT = 'https://appetize.umea-ntig.se';
 // ~ LOCAL FOR TESTING ~
-const String API_ENDPOINT = 'http://10.0.2.2:5050';
+//const String API_ENDPOINT = 'http://10.0.2.2:5050';
 
 String appetizeId = 'APPETIZE_NULL';
 
