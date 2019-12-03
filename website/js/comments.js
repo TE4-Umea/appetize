260ae5601f5e65f32877215b2560f032d4e62d25
Lacertilia <caramba97979@hotmail.com>
2019-12-03 09:56:42 +0100

    added stuffs
    Co-authored-by: Leo Sidebo <abletoirper@gmail.com>

--- website/js/comments.js
+++ website/js/comments.js
@@ -1,24 +1,26 @@
 var commentsWrapper = document.getElementById("comments");
 
-var data = {
-    comments: [
-        {
-            date: Date.now(),
-            score: 1,
-            notified_staff: false,
-            text: ["Kall mat", "Lång kö"]
-        },
-        {
-            date: Date.now(),
-            score: 2,
-            notified_staff: true,
-            text: ["Kall mat", "Lång kö"]
-        },
-        {
-            date: Date.now(),
-            score: 3,
-            notified_staff: false,
-            text: []
-        }
-    ]
-};
+var comments = [
+    {
+        date: Date.now(),
+        score: 1,
+        notified_staff: false,
+        text: ["Kall mat", "Lång kö"]
+    },
+    {
+        date: Date.now(),
+        score: 2,
+        notified_staff: true,
+        text: ["Kall mat", "Lång kö"]
+    },
+    {
+        date: Date.now(),
+        score: 3,
+        notified_staff: false,
+        text: []
+    }
+];
+
+for(var i = 0; i < comments.length; i++){
+    
+}
